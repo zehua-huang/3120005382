8d01a28458a8b20d64eaace88cac4a955c43a0ce
Michael <13682273559@163.com>
2022-09-03 14:36:44 +0800

    add Test

---
+++ Test/源.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<ctime>
+using namespace std;
+
+const double pi = 3.14;
+
+class Person1 {
+public:
+	void setName(string name) {
+		this->name = name;
+	}
+	string getName() {
+		return name;
+	}
+private:
+	string name;
+	int age;
+	string lover;
+};
+struct Person2 {
+	int number;
+};
+int main() {
+	
+}
+	
+
+
